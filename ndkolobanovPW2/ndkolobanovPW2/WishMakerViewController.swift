689dca791fa9e7f1cd0214c95a945918f8107305
nikkol615 <nik.kolobanov@bk.ru>
2025-10-07 23:38:07 +0300

    feat: add solution on HW2

---
+++ ndkolobanovPW2/ndkolobanovPW2/WishMakerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ndkolobanovPW2
+//
+//  Created by Никита Колобанов on 9/23/25.
+//
+
+import UIKit
+
+
+final class WishMakerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
